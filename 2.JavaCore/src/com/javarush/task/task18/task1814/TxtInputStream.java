a8079c759c5c10ac27d6e41633497ed7a862e820
reset1301 <reset1301@mail.ru>
2018-01-08 12:06:21 +0300

    version 1.002

--- 2.JavaCore/src/com/javarush/task/task18/task1814/TxtInputStream.java
+++ 2.JavaCore/src/com/javarush/task/task18/task1814/TxtInputStream.java
@@ -3,6 +3,7 @@ package com.javarush.task.task18.task1814;
 import java.io.FileInputStream;
 import java.io.FileNotFoundException;
 import java.io.IOException;
+import java.nio.channels.FileChannel;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
@@ -11,25 +12,107 @@ UnsupportedFileName Git
 */
 
 public class TxtInputStream extends FileInputStream {
-    TxtInputStream txtInputStream;
 
-    public TxtInputStream(String fileName) throws IOException, UnsupportedFileNameException {
+    FileInputStream component;
+
+
+
+    public TxtInputStream(String fileName) throws UnsupportedFileNameException, IOException {
+
         super(fileName);
-        Pattern p = Pattern.compile("txt$");
-        Matcher m = p.matcher(fileName);
-//        System.out.println(m.find());
-        if (m.find()){
-            
-        }
+
+
+
+        if (fileName.endsWith(".txt"))
+
+            this.component = new FileInputStream(fileName);
+
         else {
+
             super.close();
+
             throw new UnsupportedFileNameException();
+
         }
 
     }
 
-    public static void main(String[] args) throws IOException, UnsupportedFileNameException {
-//        TxtInputStream t = new TxtInputStream("c:/111/output.txt");
-    }
-}
 
+
+    @Override
+
+    public int read() throws IOException {
+
+        return this.component.read();
+
+    }
+
+
+
+    @Override
+
+    public int read(byte[] b) throws IOException {
+
+        return this.component.read(b);
+
+    }
+
+
+
+    @Override
+
+    public int read(byte[] b, int off, int len) throws IOException {
+
+        return this.component.read(b, off, len);
+
+    }
+
+
+
+    @Override
+
+    public long skip(long n) throws IOException {
+
+        return this.component.skip(n);
+
+    }
+
+
+
+    @Override
+
+    public int available() throws IOException {
+
+        return this.component.available();
+
+    }
+
+
+
+    @Override
+
+    public void close() throws IOException {
+
+        super.close();
+
+        this.component.close();
+
+    }
+
+
+
+    @Override
+
+    public FileChannel getChannel() {
+
+        return this.component.getChannel();
+
+    }
+
+
+
+    public static void main(String[] args) {
+
+    }
+
+}
