756ddf0d0f248f3445d45d913cc4c36392343366
reset1301 <reset1301@mail.ru>
2018-02-06 18:12:09 +0300

    v1.0011

---
+++ 3.JavaMultithreading/src/com/javarush/task/task21/task2108/Solution.java
@@ -0,0 +1,80 @@
+package com.javarush.task.task21.task2108;
+
+import java.util.Arrays;
+
+/*
+Клонирование растений
+*/
+public class Solution {
+    public static void main(String[] args) {
+        Tree tree = new Tree("willow", new String[]{"s1", "s2", "s3", "s4"});
+        Tree clone = null;
+        try {
+            clone = (Tree) tree.clone();
+        } catch (CloneNotSupportedException e) {
+            e.printStackTrace();
+        }
+
+        System.out.println(tree);
+        System.out.println(clone);
+
+        System.out.println(tree.branches);
+        System.out.println(clone.branches);
+    }
+
+    public static class Plant{
+        private String name;
+
+        public Plant(String name) {
+            this.name = name;
+        }
+
+        public String getName() {
+            return name;
+        }
+    }
+
+    public static class Tree extends Plant implements Cloneable{
+        private String[] branches;
+
+        public Tree(String name, String[] branches) {
+            super(name);
+            this.branches = branches;
+        }
+
+        public Tree(String name) {
+            super(name);
+        }
+
+        public String[] getBranches() {
+            return branches;
+        }
+
+        @Override
+        protected Object clone() throws CloneNotSupportedException {
+            Tree tree = new Tree(super.getName());
+            tree.branches = new String[this.getBranches().length];
+//            System.arraycopy(this.getBranches(),0,tree.branches,0,this.getBranches().length);
+            int i=0;
+            for (String s:this.getBranches()
+                 ) {
+                tree.branches[i] = s;
+                i++;
+            }
+            return tree;
+        }
+
+        @Override
+        public boolean equals(Object o) {
+            if (this == o) return true;
+            if (o == null || getClass() != o.getClass()) return false;
+            Tree tree = (Tree) o;
+            return Arrays.equals(branches, tree.branches);
+        }
+
+        @Override
+        public int hashCode() {
+            return Arrays.hashCode(branches);
+        }
+    }
+}
---
+++ 3.JavaMultithreading/src/com/javarush/task/task21/task2113/Hippodrome.java
@@ -0,0 +1,82 @@
+package com.javarush.task.task21.task2113;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Hippodrome {
+    private List<Horse> horses;
+    public static Hippodrome game;
+
+    public Hippodrome(List<Horse> horses) {
+        this.horses = horses;
+    }
+
+    public List<Horse> getHorses() {
+        return horses;
+    }
+
+    public void run() throws InterruptedException {
+        for (int i = 0; i < 100; i++) {
+            move();
+            print();
+            Thread.sleep(200);
+        }
+        Object o = new Object();
+        o.
+    }
+
+    public void move() {
+        for (Horse horse : horses
+                ) {
+            horse.move();
+        }
+    }
+
+    public void print() {
+        for (Horse horse : horses
+                ) {
+            horse.print();
+        }
+        System.out.print(System.lineSeparator() + System.lineSeparator() + System.lineSeparator()
+                + System.lineSeparator() + System.lineSeparator() + System.lineSeparator()
+                + System.lineSeparator() + System.lineSeparator() + System.lineSeparator()
+                + System.lineSeparator());
+
+    }
+
+    public Horse getWinner() {
+        Horse h = null;
+        double max = 0;
+        for (Horse horse : horses
+                ) {
+            if (horse.getDistance() > max) {
+                max = horse.getDistance();
+                h = horse;
+            }
+
+        }
+//        for (Horse horse:horses
+//             ) {
+//            if (horse.getDistance()==max) return horse;
+//        }
+        return h;
+    }
+
+    public void printWinner() {
+        System.out.println("Winner is " + getWinner().getName()+"!");
+    }
+
+    public static void main(String[] args) throws InterruptedException {
+        List<Horse> horses = new ArrayList<>();
+        horses.add(new Horse("A", 3, 0));
+        horses.add(new Horse("B", 3, 0));
+        horses.add(new Horse("C", 3, 0));
+        game = new Hippodrome(horses);
+        for (Horse h : horses
+                ) {
+            System.out.println(h.getName() + " " + h.getSpeed() + " " + h.getDistance());
+        }
+        game.run();
+        game.printWinner();
+    }
+}
