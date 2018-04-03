29b37242460dc8b16d287d2a99991a7871cb332f
reset1301 <reset1301@mail.ru>
2018-04-03 13:11:24 +0300

    v0.00020

--- 1.JavaSyntax/src/com/javarush/task/task01/task0101/Solution.java
+++ 1.JavaSyntax/src/com/javarush/task/task01/task0101/Solution.java
@@ -5,14 +5,12 @@ package com.javarush.task.task01.task0101;
 */
 
 import java.text.ParseException;
-import java.text.SimpleDateFormat;
-import java.util.Date;
 
 public class Solution {
     public static void main(String[] args) throws ParseException {
         //напишите тут ваш код
-        SimpleDateFormat simpleDateFormat = new SimpleDateFormat("dd.MM.yyyy");
-        Date date = simpleDateFormat.parse("26.03.2018");
-        System.out.println(date.getTime()/1000);
+//        SimpleDateFormat simpleDateFormat = new SimpleDateFormat("dd.MM.yyyy");
+//        Date date = simpleDateFormat.parse("26.03.2018");
+//        System.out.println(date.getTime()/1000);
     }
 }
